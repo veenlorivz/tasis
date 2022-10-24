4906808fce98d38d033792f14e8df749ff12f2f3
veenlorivz <veenlorivz194@gmail.com>
2022-10-24 09:04:45 +0700

    Initial commit

---
+++ database/migrations/2022_10_24_015535_create_kelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("nomor_kelas");
+            $table->string("nama_jurusan");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+};

bc20cb3e5c73dc69230455f60648563b3e7c51d1
gilanghadi <gilangsaputro987@gmail.com>
2022-10-31 12:14:13 +0700

    fix conflict

--- resources/views/components/content/absen/x/index.blade.php
+++ resources/views/components/content/absen/x/index.blade.php
@@ -6,87 +6,65 @@
     Data Absensi
 @endsection
 @section('content')
-    <div class="accordion shadow-sm rounded-lg" id="accordionPanelsStayOpenExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                    aria-controls="panelsStayOpen-collapseOne">
-                    Pemasaran
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                    <p class="ms-3" style="color: rgb(143, 142, 142);">Pemasaran | Absen</p>
-                    <table class="table">
-                        <thead class="text-center">
-                            <tr>
-                                <th rowspan="2" scope="col">No</th>
-                                <th rowspan="2" scope="col">Nama Siswa</th>
-                                <th rowspan="2" scope="col">Nis</th>
-                                <th rowspan="2" scope="col">Izin</th>
-                                <th rowspan="2" scope="col">Sakit</th>
-                                <th rowspan="2" scope="col">Alpha</th>
-                                <th rowspan="2" scope="col">Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($siswa as $s)
-                                <tr>
-                                    <th scope="row">{{ $loop->iteration }}</th>
-                                    <td>{{ $s->nama }}</td>
-                                    <td>{{ $s->nis }}</td>
-                                    <td>{{ $s->izin }}</td>
-                                    <td>{{ $s->sakit }}</td>
-                                    <td>{{ $s->alpha }}</td>
-                                    <td>{{ $s->izin + $s->sakit + $s->alpha }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+    @foreach ($bulan as $b)
+        <div class="mb-4">
+            <h3 class="ms-2 mb-3 mt-5">{{ $b }}</h3>
+            <hr>
+        </div>
+        @foreach ($kelas as $k)
+            <div class="accordion shadow-sm rounded-lg" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="{{ "s$k->id$b" }}-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#{{ "s$k->id$b" }}-collapseTwo" aria-expanded="false"
+                            aria-controls="{{ "s$k->id$b" }}-collapseTwo">
+                            {{ "$k->nomor_kelas  $k->nama_jurusan" }}
+                        </button>
+                    </h2>
+                    <div id="{{ "s$k->id$b" }}-collapseTwo" class="accordion-collapse collapse"
+                        aria-labelledby="{{ "s$k->id$b" }}-headingTwo">
+                        <div class="accordion-body">
+                            <table class="table">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th rowspan="2" scope="col">No</th>
+                                        <th rowspan="2" scope="col">Nama Siswa</th>
+                                        <th rowspan="2" scope="col">Nis</th>
+                                        <th rowspan="2" scope="col">Izin</th>
+                                        <th rowspan="2" scope="col">Sakit</th>
+                                        <th rowspan="2" scope="col">Alpha</th>
+                                        <th rowspan="2" scope="col">Total</th>
+                                        <th scope="col">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    @foreach ($k->siswa as $siswa)
+                                        <tr>
+                                            <th scope="row">{{ $loop->iteration }}</th>
+                                            <td>{{ $siswa->nama }}</td>
+                                            <td>{{ $siswa->nis }}</td>
+                                            <td>{{ $siswa->izin }}</td>
+                                            <td>{{ $siswa->sakit }}</td>
+                                            <td>{{ $siswa->alpha }}</td>
+                                            <td>{{ $siswa->izin + $siswa->sakit + $siswa->alpha }}</td>
+                                            <td>
+                                                <a href="" class="text-decoration-none">
+                                                    <i
+                                                        class="bi bi-plus-circle-fill text-info fa-2x mr-2 cursor-pointer text-decoration-none"></i>
+                                                </a>
+                                                <a href="" class="text-decoration-none">
+                                                    <i
+                                                        class="bi bi-arrow-right-circle-fill text-info fa-2x cursor-pointer "></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
-                    aria-controls="panelsStayOpen-collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingTwo">
-                <div class="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                    modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                    does limit overflow.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
-                    aria-controls="panelsStayOpen-collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                    modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                    does limit overflow.
-                </div>
-            </div>
-        </div>
-    </div>
+        @endforeach
+    @endforeach
 @endsection

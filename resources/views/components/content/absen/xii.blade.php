8de832ff7142e280249f97e4f69e59f435114d29
gilanghadi <gilangsaputro987@gmail.com>
2022-10-31 11:01:17 +0700

    according

--- resources/views/components/content/absen/xii.blade.php
+++ resources/views/components/content/absen/xii.blade.php
@@ -6,54 +6,65 @@
     Data Absensi
 @endsection
 @section('content')
-    <div class="card shadow-sm rounded-lg">
-        <div class="card-title">
-            <h5 class="pt-4 ml-4 mb-4">Absensi | XII</h5>
+    @foreach ($bulan as $b)
+        <div class="mb-4">
+            <h3 class="ms-2 mb-3 mt-5">{{ $b }}</h3>
             <hr>
         </div>
-        <div class="card-body">
-            <table class="table" style="margin-top: -20px;" border="1">
-                <thead class="text-center">
-                    <tr>
-                        <th rowspan="2" scope="col">No</th>
-                        <th rowspan="2" scope="col">Nama Siswa</th>
-                        <th rowspan="2" scope="col">Jurusan</th>
-                        <th>Izin</th>
-                        <th>Sakit</th>
-                        <th>Alpha</th>
-                        <th rowspan="2" scope="col">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry the Bird</td>
-                        <td>Larry the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+        @foreach ($kelas as $k)
+            <div class="accordion shadow-sm rounded-lg" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="{{ "s$k->id$b" }}-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#{{ "s$k->id$b" }}-collapseTwo" aria-expanded="false"
+                            aria-controls="{{ "s$k->id$b" }}-collapseTwo">
+                            {{ "$k->nomor_kelas  $k->nama_jurusan" }}
+                        </button>
+                    </h2>
+                    <div id="{{ "s$k->id$b" }}-collapseTwo" class="accordion-collapse collapse"
+                        aria-labelledby="{{ "s$k->id$b" }}-headingTwo">
+                        <div class="accordion-body">
+                            <table class="table">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th rowspan="2" scope="col">No</th>
+                                        <th rowspan="2" scope="col">Nama Siswa</th>
+                                        <th rowspan="2" scope="col">Nis</th>
+                                        <th rowspan="2" scope="col">Izin</th>
+                                        <th rowspan="2" scope="col">Sakit</th>
+                                        <th rowspan="2" scope="col">Alpha</th>
+                                        <th rowspan="2" scope="col">Total</th>
+                                        <th scope="col">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    @foreach ($k->siswa as $siswa)
+                                        <tr>
+                                            <th scope="row">{{ $loop->iteration }}</th>
+                                            <td>{{ $siswa->nama }}</td>
+                                            <td>{{ $siswa->nis }}</td>
+                                            <td>{{ $siswa->izin }}</td>
+                                            <td>{{ $siswa->sakit }}</td>
+                                            <td>{{ $siswa->alpha }}</td>
+                                            <td>{{ $siswa->izin + $siswa->sakit + $siswa->alpha }}</td>
+                                            <td>
+                                                <a href="" class="text-decoration-none">
+                                                    <i
+                                                        class="bi bi-plus-circle-fill text-info fa-2x mr-2 cursor-pointer text-decoration-none"></i>
+                                                </a>
+                                                <a href="" class="text-decoration-none">
+                                                    <i
+                                                        class="bi bi-arrow-right-circle-fill text-info fa-2x cursor-pointer "></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endforeach
 @endsection

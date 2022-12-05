fe2b78ab158bd6d7d7d407f1ab678320c2baa355
gilanghadi <gilangsaputro987@gmail.com>
2022-12-05 14:19:22 +0700

    del seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([KelasSeeder::class, SiswaSeeder::class, UserSeeder::class]);
+        $this->call([KelasSeeder::class, SiswaSeeder::class]);
     }
 }

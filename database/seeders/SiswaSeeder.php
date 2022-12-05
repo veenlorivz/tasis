d601d338dee54fea059f8d645b566a15140fcfff
gilanghadi <gilangsaputro987@gmail.com>
2022-12-05 14:37:52 +0700

    seed del

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -14,29 +14,5 @@ class SiswaSeeder extends Seeder
      */
     public function run()
     {
-        $siswa = [
-            [
-                "nama" => "Andi Suhadi",
-                "nis" => "00012831",
-                "kelas_id" => 1,
-                "poin" => 150,
-            ],
-            [
-                "nama" => "Kevin Sanjaya",
-                "nis" => "000318632",
-                "kelas_id" => 2,
-                "poin" => 150,
-            ],
-            [
-                "nama" => "Usep Sujajang",
-                "nis" => "000321768",
-                "kelas_id" => 3,
-                "poin" => 150,
-            ],
-        ];
-
-        foreach ($siswa as $s) {
-            \App\Models\Siswa::create($s);
-        }
     }
 }
